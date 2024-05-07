18a16df8f3324be443330c6593f2f3ffd0baabef
NikitaMikhailovich <nikita.gaynov@mail.ru>
2024-05-08 01:38:53 +0300

    account forms bug fix

--- src/api/filters.py
+++ src/api/filters.py
@@ -2,8 +2,7 @@ from django_filters.rest_framework import FilterSet, filters
 
 from board.models import Vacancy
 
-# почему не можем просто использовать мета класс?
-# надо написать фильтр с def
+
 class VacancyFilter(FilterSet):
     company_id = filters.NumberFilter(field_name="company")
     specialization_id = filters.NumberFilter(field_name="specialization")
